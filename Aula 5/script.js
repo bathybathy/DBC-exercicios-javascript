b5688fe03295ad3b4e2225fcb559f3d1581d52fd
Nathalia Duarte <duartenatha@gmail.com>
2022-03-13 13:45:21 -0300

    finalizado?

--- Aula 5/script.js
+++ Aula 5/script.js
@@ -8,16 +8,17 @@ const cadastrarProduto = (id, descricao, preco) =>{
     id = contadorId;
     descricao = prompt("Entre a descrição do produto:");
     preco = prompt("Digite o preço do produto:");
-    if(isNaN(descricao) || descricao > 0 || descricao === null || descricao === undefined){
+    if(descricao > 0 || descricao === null || descricao === undefined || descricao === ""){
         alert("Entre uma descrição válida.")
         contaErro++
-    }else if(isNaN(preco) || preco === null || preco === undefined || preco < 0){
+    }else if(isNaN(preco) || preco === null || preco === undefined || preco < 0 || preco === ""){
         alert("Entre um preço válido.")
         contaErro++
     }
     if(contaErro === 0){
         produtoLista.push({id, descricao, preco})
         alert("Cadastro realizado com sucesso.")
+        console.log(produtoLista)
     }
     
     return produtoLista
@@ -26,14 +27,20 @@ const cadastrarProduto = (id, descricao, preco) =>{
 
 const deletarPorId = (deletar) =>{
     deletar = prompt("entre o número de cadastro do produto que deseja deletar");
-    if (deletar === null || !produtoLista.some( () => deletar.id === produtoLista.id)){
+    if (deletar === null || deletar === "" || !produtoLista.some( () => deletar.id === produtoLista.id)){
         alert("Digite um número válido. Operação cancelada.")
     }else{  
         let confirmacao = confirm(`tem certeza que deseja deletar o produto de número ${deletar}?`)
         if(confirmacao){
-            let deletarIndex = produtoLista.indexOf(produtoLista.id);
+            deletar = parseInt(deletar);
+            let acharProduto = produtoLista.find(el => el.id === deletar);
+            console.log(acharProduto)
+            let deletarIndex = produtoLista.indexOf(acharProduto)
+            console.log(deletarIndex)
+            // deletarIndex = deletarIndex.indexOf();
             produtoLista.splice(deletarIndex, 1);
             alert("Produto deletado do cadastro.");
+            console.log(produtoLista)
         }else{
             console.log(produtoLista);
             alert("Operação cancelada.")
@@ -58,6 +65,7 @@ const encontrarDescricao = () =>{
     let descricaoEncontrada = [];
     if(produtoLista.some (el => el.descricao === produtoLista.includes(acharDescricao))){
         descricaoEncontrada = produtoLista.filter ( el => el.descricao.includes(acharDescricao));
+        alert("Verifique o console para as informações desejadas.")
         console.log(descricaoEncontrada);
     }else{
         alert("Descrição não encontrada. Operação cancelada.")
@@ -69,8 +77,9 @@ tentei fazer uma função pra mapa de preço, mas quando chamo dentro de outra f
 
 const patrimonioTotal = () => {
     let precosMapa = produtoLista.map(function(item) { return item ["preco"] });
+    
     console.log(precosMapa)
-    let soma = precosMapa.reduce ((n1, n2) => n1+n2, 0)
+    let soma = precosMapa.reduce ((n1, n2) => parseFloat(n1) + parseFloat(n2), 0)
     alert(`O patrimônio total é de ${soma} reais.`)
 }
 
@@ -88,34 +97,34 @@ const validarNumeros = () => {
         }
     })
     if(contaErro = 0){
-        alert("Todos preços cadastrados possuem valores válidos")
+        alert("Todos preços cadastrados possuem valores válidos.")
     }else{
         alert("Há preços com valores não válidos.")
     }
 }
 
-let menu = 0;
+let menu;
 let acharDescricao;
 let opcao;
 do{
-    menu = parseInt(prompt("Digite 1 para cadastrar um produto.\nDigite 2 para excluir um produto pelo número de cadastro.\nDigite 3 para encontrar um produto pelo número de cadastro.\nDigite 4 para menu de tabelas.\nDigite 5 para mostrar o patrimônio total.\nDigite 6 para confirmar se os preços cadastrados são válidos.\nDigite 7 se deseja sair do menu."));
+    menu = prompt("Digite 1 para cadastrar um produto.\nDigite 2 para excluir um produto pelo número de cadastro.\nDigite 3 para encontrar um produto pelo número de cadastro.\nDigite 4 para menu de tabelas.\nDigite 5 para mostrar o patrimônio total.\nDigite 6 para confirmar se os preços cadastrados são válidos.\nDigite 7 se deseja sair do menu.");
     
     switch(menu){
-        case 1: 
+        case "1": 
             cadastrarProduto();
             console.log(produtoLista);
             break;
         
-        case 2:
+        case "2":
             deletarPorId(produtoLista.id);
             console.log(produtoLista);
             break;
 
-        case 3:
+        case "3":
             encontrarProdutoId(produtoLista.id);
             break;
 
-        case 4:
+        case "4":
             opcao = parseInt(prompt("Digite 1 para imprimir a tabela completa.\nDigite 2 para imprimir a tabela por descrição.\nDigite 3 para buscar uma descrição de produto e imprimi-la junto com seus preços."))
             if (opcao === 1){
                 console.table(produtoLista);
@@ -132,26 +141,32 @@ do{
             } 
             break;
         
-        case 5:
+        case "5":
             patrimonioTotal();
             break;
 
-        case 6:
+        case "6":
             validarNumeros()
             break;
         
-        case 7:
+        case "7":
             let sair = confirm("Realmente deseja sair?");
             if(sair){
-                menu = 8;
-                alert("O menu será fechado")
+                menu = "7";
+                alert("O menu será fechado.")
                 break;
             }else{
+                menu = "0";
                 break;
             }
 
+        case null:
+            menu = "7";
+            alert("O menu será fechado.");
+            break;
+
         default: 
-            alert("Entre uma opção válida")
+            alert("Entre uma opção válida.")
     }
-}while(menu !== 8)
+}while(menu !== "7")
 
